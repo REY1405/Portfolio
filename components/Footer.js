537b4399ee50a03bc3b96d4f26aca3cf0f4d38e3
REY1405 <kathal.rishi@gmail.com>
2025-12-08 16:12:59 +0530

    added testing code

--- components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-  return (
-    <footer className="mt-20 py-12 text-center text-sm text-slate-500">
-      © {new Date().getFullYear()} Rishi Kathal — Built with ❤️ • Hosted on Vercel
-    </footer>
-  )
-}
